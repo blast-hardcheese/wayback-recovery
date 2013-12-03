175a22e2580af5a7bb560c5bcecfc1f8e2d02396
Devon Stewart <blast@hardchee.se>
2013-12-03 12:53:11 -0800

    Screwed up regex-- used s/// instead of s~~~

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
-ack -ho '/web/\d+[^\/]*\/http:\/\/amysnively\.com\/[^"'\'']+' "$1" | perl -ne 'chomp; $o=$_; s~^.*\.com/~~; print "echo Getting \"$_\"; mkdir -p \$\(dirname \"$_\"\); [ -e \"$_\" ] \|\| curl -L \"https://web.archive.org$o\" -o \"$_\"; sed -i \"\" '\''s/$o/$_'\'' \"'$1'\"\n";' | tee script.sh
+ack -ho '/web/\d+[^\/]*\/http:\/\/amysnively\.com\/[^"'\'']+' "$1" | perl -ne 'chomp; $o=$_; s~^.*\.com/~~; print "echo Getting \"$_\"; mkdir -p \$\(dirname \"$_\"\); [ -e \"$_\" ] \|\| curl -L \"https://web.archive.org$o\" -o \"$_\"; sed -i \"\" '\''s~$o~$_~'\'' \"'$1'\"\n";' | tee script.sh
 
 echo "Wrote script.sh"
